788fd6d019c88cd2849675429c25cc5c41583089
Mathieu <unknown-archi@users.noreply.github.com>
2024-08-03 12:58:58 +0200

    Save

---
+++ mako.nix
@@ -0,0 +1,13 @@
+{
+  services.mako = {
+    backgroundColor = "#3c3836";
+    borderColor = "#b16286";
+    borderRadius = 6;
+    borderSize = 2;
+    defaultTimeout = 5000;
+    enable = true;
+    font = "Iosevka 12";
+    layer = "overlay";
+    textColor = "#ebdbb2";
+  };
+}
