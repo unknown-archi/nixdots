d0a95bada543f67d77db2c2a780cffba494212f1
Mathieu <unknown-archi@users.noreply.github.com>
2024-08-22 18:41:37 +0200

    Moved let-in for zen

--- configuration.nix
+++ configuration.nix
@@ -112,12 +112,14 @@
   nixpkgs.config.allowUnfree = true;
   
 
-  let 
-    zen-flake = builtins.getFlake(toString ./.);
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
- # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  environment.systemPackages = let
+
+  zen-flake = builtins.getFlake (toString ./.);
+
+    in with pkgs; [
+    # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     unzip
     curl
