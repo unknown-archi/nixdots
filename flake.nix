33e1b185c25dbf40445b5160e7bd85e19cfe3119
Mathieu <unknown-archi@users.noreply.github.com>
2024-08-22 18:31:43 +0200

    Added Zen Browser to config

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
+
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
