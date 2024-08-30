88f5cce268c83200a480162ef9f11e3ad76ba637
Mathieu <unknown-archi@users.noreply.github.com>
2024-08-30 19:57:08 +0200

    Changed the red color

--- wezterm.lua
+++ wezterm.lua
@@ -23,8 +23,8 @@ config.colors = {
 	cursor_fg = "#011423",
 	selection_bg = "#033259",
 	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	ansi = { "#214969", "#ed2d53", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#ed2d53", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
 -- and finally, return the configuration to wezterm
