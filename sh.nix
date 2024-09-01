0e5312ecddaabcd21de8efc2ed5bdeb60bc7abc2
Mathieu <unknown-archi@users.noreply.github.com>
2024-09-01 20:20:29 +0200

    Added back autocompletion thing

--- sh.nix
+++ sh.nix
@@ -58,6 +58,18 @@
     };
 
     initExtra = ''
+	
+	# Better autocompletion
+	HISTFILE=$HOME/.zhistory
+	SAVEHIST=1000
+	HISTSIZE=999
+	setopt share_history
+	setopt hist_expire_dups_first
+	setopt hist_ignore_dups
+	setopt hist_verify
+	bindkey '^[[A' history-search-backward
+	bindkey '^[[B' history-search-forward	
+
 	# My functions
 	save() {
 		if [ -z "$1" ]; then
