04a363ae402db540b937f887dda74971479fe412
Mathieu <unknown-archi@users.noreply.github.com>
2024-09-18 19:47:02 +0200

    Changed color in the theme

--- home.nix
+++ home.nix
@@ -98,7 +98,7 @@
   home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;  
 
   # Bat theme
-  # home.file.".config/bat/themes/tokyonight.tmTheme".source = ./tokyonight.tmTheme;
+   home.file.".config/bat/themes/custom.tmTheme".source = ./custom.tmTheme;
 
   # Virt manager
   dconf.settings = {
