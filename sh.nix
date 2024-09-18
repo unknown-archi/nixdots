49e5bca49e1a16c7bf463eec754d5d3178837859
Mathieu <unknown-archi@users.noreply.github.com>
2024-09-18 19:02:49 +0200

    Added fd

--- sh.nix
+++ sh.nix
@@ -187,6 +187,24 @@
 
 	# Fzf
 	eval "$(fzf --zsh)"
+	
+	# -- Use fd instead of fzf --
+
+	export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix"
+	export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+	export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix"
+
+	# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+	# - The first argument to the function ($1) is the base path to start traversal
+	# - See the source code (completion.{bash,zsh}) for the details.
+	_fzf_compgen_path() {
+	  fd --hidden --exclude .git . "$1"
+	}
+
+	# Use fd to generate the list for directory completion
+	_fzf_compgen_dir() {
+	  fd --type=d --hidden --exclude .git . "$1"
+	}
     '';
 
   };
