aa9e7910d04fd03b2e1c5af79d78e381e39b8282
Mathieu <unknown-archi@users.noreply.github.com>
2024-09-26 20:52:38 +0200

    Trying to install xdph

--- home.nix
+++ home.nix
@@ -29,7 +29,7 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     pkgs.hello
-    pkgs.hyprland.packages.${pkgs.system}.xdph
+    hyprland.xdph
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
