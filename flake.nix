2ca4fdedd32fcbdc1acaf1cf9033cc296418e8d2
Mathieu <unknown-archi@users.noreply.github.com>
2024-09-26 23:24:54 +0200

    Added cursor as input

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
