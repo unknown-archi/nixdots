ca666b7baa3eb21b92c29f15a0888d2fbdc3179c
Mathieu <unknown-archi@users.noreply.github.com>
2024-11-01 18:38:37 +0100

    Added alliases for fix-python

--- term/sh.nix
+++ term/sh.nix
@@ -58,7 +58,7 @@
       tree="eza --icons --tree";
       wezterm="WAYLAND_DISPLAY=wayland-0 wezterm";
       alpine="sudo docker run -it --mount type=bind,source=/home/mathieu/VMs/alpine_docker/alpine_data,target=/mydata my_alpine";
-      fix_python="fix-python --venv .venv"
+      fix_python="fix-python --venv .venv";
     };
 
     initExtra = ''
