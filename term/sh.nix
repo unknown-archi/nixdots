26de140ce3ea85c4e744d5b2b859fd99d1cc1a74
Mathieu <unknown-archi@users.noreply.github.com>
2024-11-10 19:59:58 +0100

    Added alias for superfile

--- term/sh.nix
+++ term/sh.nix
@@ -60,6 +60,7 @@
       alpine="sudo docker run -it --mount type=bind,source=/home/mathieu/VMs/alpine_docker/alpine_data,target=/mydata my_alpine";
       fix_python="fix-python --venv .venv";
       open_resume="docker run -p 3000:3000 open-resume";
+      spf="superfile";
     };
 
     initExtra = ''
