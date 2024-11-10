f23ff87bd622d6da751ce49cbd7255b16f516ee2
Mathieu <unknown-archi@users.noreply.github.com>
2024-11-10 19:53:00 +0100

    Added superfile flake input

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    superfile = {
+      url = "github:yorukot/superfile";
+    };
 
     rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
   };
