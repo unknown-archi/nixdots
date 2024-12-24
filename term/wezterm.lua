c152dd8af66181a8f9b9a20c7de99bf2984a0549
Mathieu <unknown-archi@users.noreply.github.com>
2024-12-24 19:02:56 +0100

    Removed terminal font

--- term/wezterm.lua
+++ term/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+-- config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 13
 
 config.enable_tab_bar = false
