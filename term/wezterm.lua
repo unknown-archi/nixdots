06f5afcf8ec3cbb2dc39e9a11b8dc271ab03dfe4
Mathieu <unknown-archi@users.noreply.github.com>
2024-12-24 19:20:18 +0100

    Trying to fix wezterm

--- term/wezterm.lua
+++ term/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font("Fira Code Mono", {weight="Light"})
+config.font = wezterm.font("Fira Code Nerd Font Mono", {weight="Light"})
 config.font_size = 13
 config.enable_tab_bar = false
 config.window_background_opacity = 0.8
