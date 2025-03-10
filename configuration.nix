2a615917a7e53c962734f3bcb1d82e6555f1e4e1
Mathieu <unknown-archi@users.noreply.github.com>
2025-03-11 00:18:32 +0100

    VM network fix

--- configuration.nix
+++ configuration.nix
@@ -353,8 +353,8 @@
 
   # Specify the QEMU package
   virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
-  networking.ipv4.forwarding = true;
-    
+  boot.kernel.sysctl."net.ipv4.ip_forward" = "1";
+  
   # (Optional) Additional libvirtd configuration
   virtualisation.libvirtd.extraConfig = ''
     unix_sock_group = "libvirtd"
